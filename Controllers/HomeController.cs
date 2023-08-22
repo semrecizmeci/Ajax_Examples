3d4bc2a2710e1f900cbaafb13d1f6a3c95a3384d
Sadık Emre Çizmecioğlu <121570540+semrecizmeci@users.noreply.github.com>
2023-08-22 15:07:24 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using ajaxDeneme.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ajaxDeneme.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        //public class datas
+        //{
+        //    public int id { get; set; }
+        //    public string? name { get; set; }
+        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(int id, string name)
+        {
+            ViewBag.data = $"{name}"+$"{id}";
+        
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
